20ab7ac3a714eb34a2e630e36cb8e0f4c71d2426
Kristian Pratama Nainggolan <kristian.nainggolan@julofinance.com>
2024-09-06 12:11:46 +0700

    fill the code

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,13 @@
-
+def doFibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    
+    a = 0
+    b = 1
+    for i in range(2 , n + 1):
+        temp = a + b
+        a = b
+        b = temp
+    return temp
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-import fibonacci
+from fibonacci import doFibonacci
+
+def main():
+    number = int(input("Input the the number: "))
+    result = doFibonacci(number)
+    print(f"The Fibonacci sequence is :  {result}" )
+
+
+
+if __name__ == "__main__":
+    main()
